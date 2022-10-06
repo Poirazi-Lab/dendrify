dc6fc97a6d5c8ca1b2fccb32cbda44382388ca52
Michalis Pagkalos <mpagkalos93@gmail.com>
2022-10-06 19:47:41 +0300

    rtd fix

--- dendrify/compartment.py
+++ dendrify/compartment.py
@@ -259,7 +259,8 @@ class Compartment:
 
     def noise(self, tau: Quantity = 20*ms, sigma: Quantity = 3*pA, mean: Quantity = 0*pA):
         """
-        Adds a stochastic noise current.
+        Adds a stochastic noise current. For more information see the Noise
+        section: of :doc:`brian2:user/models`
 
         Parameters
         ----------
