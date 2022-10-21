93e229ade7fe4aa5af58ebd02591414bd61ffb5b
Christos Karageorgiou Kaneen <ckarageorgkaneen@gmail.com>
2022-10-21 14:43:54 +0300

    Add install requirement

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['brian2==2.5.1'],
     keywords=['python', 'brian2', 'dendrites', 'SNNs', 'network models'],
     classifiers=[
         "Development Status :: 4 - Beta",
