d4a8e4114d3ed50dd8a812c93811a5423d4d9e95
Spiros Chavlis <spirosChv@users.noreply.github.com>
2023-07-07 12:55:51 +0300

    Minor fix (#23)
    * fix: mvolt import
    
    Brian automated use `mvolt` instead of `mV`.
    
    * Update setup.py
    
    * Update conf.py

--- docs_sphinx/source/conf.py
+++ docs_sphinx/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../../..'))
 project = 'Dendrify'
 copyright = '2022, Michalis Pagkalos'
 author = 'Michalis Pagkalos'
-release = '1.0.8'
+release = '1.0.9'
 
 # -- General configuration -----------------------------------------------------
 extensions = [
