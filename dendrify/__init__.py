9be157e384b78f001713aee42d59798509262eeb
Michalis Pagkalos <mpagkalos93@gmail.com>
2023-07-10 14:04:33 +0300

    Added PointNeuronModel to init

--- dendrify/__init__.py
+++ dendrify/__init__.py
@@ -1,4 +1,4 @@
 from .compartment import Compartment, Dendrite, Soma
 from .ephysproperties import EphysProperties, default_params
 from .equations import library
-from .neuronmodel import NeuronModel
+from .neuronmodel import NeuronModel, PointNeuronModel
