2188357b4ae29a46d9b710454244fbecdcf1a154
Michalis Pagkalos <30598350+mpgl@users.noreply.github.com>
2024-03-13 15:20:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = '2.1.0'
+VERSION = '2.1.1'
 DESCRIPTION = 'A package for adding dendrites to SNNs'
 with open("README.rst") as f:
     LONG_DESCRIPTION = f.read()
